2d7f591684d90ae2ab43e8b4dcf2d0223b8331f6
Kauembergy Ferreira Diniz <162308950+kauembergy@users.noreply.github.com>
2024-06-26 14:21:25 -0300

    Update personalizado.py
    adicionei import calcular

--- personalizado.py
+++ personalizado.py
@@ -4,6 +4,7 @@ from customtkinter import CTkFont
 from tkinter import messagebox
 import numpy as np
 import pandas as pd
+from calculo import calcular
 
 # Definir tema e cores
 customtkinter.set_appearance_mode("dark")
